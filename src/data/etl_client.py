079bfb052665d984085ab46b562044ee0f00ab0d
Richard Ogoma <richard.ogoma@outlook.com>
2023-06-24 19:33:17 +0100

    Update etl_client.py
    Improved code documentation

--- src/data/etl_client.py
+++ src/data/etl_client.py
@@ -8,7 +8,7 @@ from config import Config
 config = Config()
 
 
-def run_etl() -> bool:
+def run_etl():
     """
     Conditionally run the data streaming client as a subprocess.
     """
@@ -16,22 +16,25 @@ def run_etl() -> bool:
         # Change the working directory
         project_home = config.get_config_value("PROJECT_HOME")
         etl_directory = config.get_config_value("ETL_DIRECTORY")
+        etl_entrypoint = config.get_config_value("ETL_ENTRYPOINT")
         os.chdir(etl_directory)
 
         # Start data streaming client
-        etl_entrypoint = config.get_config_value("ETL_ENTRYPOINT")
         result = subprocess.run(["bash", etl_entrypoint], check=True)
 
+        # Return the exit code of the subprocess
         etl_exitcode = result.returncode
+
+        # Step out of the etl directory
+        os.chdir(project_home)
+        
         if etl_exitcode == 0:
             print(r"Data Streaming Client running ...")
-            os.chdir(project_home)
 
         else:
             print(
                 f"Data Streaming Client execution failed with an exit status of {etl_exitcode}"
             )
-            os.chdir(project_home)
             sys.exit(etl_exitcode)
 
     except FileNotFoundError:
